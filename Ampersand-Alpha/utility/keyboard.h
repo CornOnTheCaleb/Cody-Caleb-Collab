6e4718b73da9afedcd8daba4d4536edee2b2d198
CornOnTheCaleb <caberg1998@gmail.com>
2017-11-01 21:31:48 -0500

    Add keyboard.h and create utility header file directory

---
+++ Ampersand-Alpha/utility/keyboard.h
@@ -0,0 +1,52 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/select.h>
+#include <termios.h>
+#include <stropts.h>
+#include <iostream>
+
+using namespace std;
+
+int khbit ()
+{
+  timeval tv;
+  fd_set fds;
+  tv.tv_sec = 0;
+  tv.tv_usec = 0;
+  FD_ZERO(&fds);
+  FD_SET(STDIN_FILENO, &fds);
+  select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
+  return FD_ISSET(STDIN_FILENO, &fds);
+}
+
+void nonblock (const bool state)
+{
+  termios ttystate;
+  tcgetattr(STDIN_FILENO, &ttystate);
+  if (state == true)
+  {
+    ttystate.c_lflag &= (~ICANON & ~ECHO); //Not display character
+    ttystate.c_cc[VMIN] = 1;
+  }
+  else if (state == false)
+  {
+    ttystate.c_lflag |= ICANON | ECHO;
+  }
+  tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
+  return;
+}
+
+char getch ()
+{
+  if (khbit() != 0)
+  {
+    char c = fgetc(stdin);
+    return c;
+  }
+  return '\0';
+}
+
+#endif
