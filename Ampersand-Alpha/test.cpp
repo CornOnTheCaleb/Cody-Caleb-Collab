919b1edb8786206f77e37777e3890432b3e205ea
Cody Less <oeyman19@hotmail.com>
2017-11-11 00:09:38 +0000

    Fixed bugs in character.h and added test.cpp

---
+++ Ampersand-Alpha/test.cpp
@@ -0,0 +1,11 @@
+#include "character.h"
+#include "utility/printmanager.h"
+
+int main()
+{
+  PrintManager tmp;
+  World fuck;
+  cout << "whoops" << endl;
+  tmp.smart_print(fuck, 1, 1);
+  return 0;
+}
